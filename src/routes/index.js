8c00cf8d5fbb04a407a9a45c6d15ffe203c1c98d
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2025-05-16 01:50:50 +0100

    ♻️ refactor: Definir rotas publicas e privadas para endpoint de vagas

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,15 @@ import utilizadoresRoutes from '../utilizadores/utilizadores.routes.js';
 import produtosRoutes from '../produtos/produtos.routes.js';
 import clientesRoutes from '../clientes/clientes.routes.js';
 import fornecedoresRoutes from '../fornecedores/fornecedores.routes.js';
+import vagasRoutes from '../vagas/vagas.routes.js';
+import candidaturasRoutes from '../candidaturas/candidaturas.routes.js';
 import authRoutes from '../auth/auth.routes.js';
 import { authMiddleware } from '../middlewares/index.js';
 const router = express.Router();
 
 router.use('/auth', authRoutes);
+router.use('/vagas', vagasRoutes);
+router.use('/candidaturas', candidaturasRoutes);
 router.use(authMiddleware);
 router.use('/produtos', produtosRoutes);
 router.use('/fornecedores', fornecedoresRoutes);
--- src/vagas/vagas.routes.js
+++ src/vagas/vagas.routes.js
@@ -1,10 +1,11 @@
-// src/routes/vagas.routes.js
 import express from 'express';
 const router = express.Router();
 import vagasController from './vagas.controller.js';
+import { authMiddleware } from '../middlewares/index.js';
 
 router.get('/', vagasController.getAll);
 router.get('/:vagaId', vagasController.getById);
+router.use(authMiddleware);
 router.post('/cadastrar', vagasController.create);
 router.put('/actualizar/:vagaId', vagasController.update);
 router.delete('/apagar/:vagaId', vagasController.deleteVaga);
